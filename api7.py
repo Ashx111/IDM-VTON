9e46a892d67dc77316d19705431c839320a9c64f
Ashirvad Singh <141310930+Ashx111@users.noreply.github.com>
2025-01-20 17:20:25 +0530

    Update api7.py

--- api7.py
+++ api7.py
@@ -1,6 +1,7 @@
 import torch
 from flask import Flask
 import gc
+import os
 from torchvision import transforms
 from src.tryon_pipeline import StableDiffusionXLInpaintPipeline as TryonPipeline
 from src.unet_hacked_garmnet import UNet2DConditionModel as UNet2DConditionModel_ref
@@ -12,8 +13,8 @@ from transformers import (
 from diffusers import AutoencoderKL
 from preprocess.humanparsing.run_parsing import Parsing
 from preprocess.openpose.run_openpose import OpenPose
-from util.pipeline import quantize_4bit
-from torch_int_linear import BLinear4bit as KBitLinear
+from util.pipeline import quantize_4bit  # Keep the import, might be used internally by other libs
+from torch_int_linear import BLinear8bit as KBitLinear  # Assuming you use this for 8-bit
 
 app = Flask(__name__)
 
@@ -22,23 +23,29 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 pipe = None
 unet = None
 UNet_Encoder = None
-parsing_model = None  # Initialize globally
-openpose_model = None # Initialize globally
+parsing_model = None
+openpose_model = None
 tensor_transfrom = transforms.Compose(
-                    [
-                        transforms.ToTensor(),
-                        transforms.Normalize([0.5], [0.5]),
-                    ]
-            )
+    [
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5]),
+    ]
+)
+
+load_mode = '8bit'  # Force 8-bit quantization
+fixed_vae = True
+
+def quantize_8bit(model):
+    # Your 8-bit quantization logic here
+    for n, m in model.named_modules():
+        if isinstance(m, torch.nn.Linear):
+            torch.nn.utils.parametrize.register_parametrization(m, 'weight', KBitLinear(m.in_features, m.out_features))
 
 def load_models():
-    print("****loading models****")
     global pipe, unet, UNet_Encoder, parsing_model, openpose_model
+    print("****loading models****")
     dtype = torch.float16
-    dtypeQuantize = dtype
-    load_mode = '8bit' # Or whatever your desired load mode is
-    if(load_mode in ('4bit','8bit')):
-        dtypeQuantize = torch.float8_e4m3fn
+    dtypeQuantize = torch.float8_e4m3fn  # Assuming this is correct for your 8-bit setup
 
     model_id = 'yisol/IDM-VTON'
     vae_model_id = 'madebyollin/sdxl-vae-fp16-fix'
@@ -47,34 +54,33 @@ def load_models():
         model_id,
         subfolder="unet",
         torch_dtype=dtypeQuantize,
-    ).to(device) # Move to device here
-    if load_mode == '4bit':
-        quantize_4bit(unet)
+    ).to(device)
+    if load_mode == '8bit':
+        quantize_8bit(unet)
     unet.requires_grad_(False)
 
     image_encoder = CLIPVisionModelWithProjection.from_pretrained(
         model_id,
         subfolder="image_encoder",
         torch_dtype=torch.float16,
-    ).to(device) # Move to device here
-    if load_mode == '4bit':
-        quantize_4bit(image_encoder)
+    ).to(device)
+    if load_mode == '8bit':
+        quantize_8bit(image_encoder)
 
-    if True:
-        vae = AutoencoderKL.from_pretrained(vae_model_id, torch_dtype=dtype).to(device) # Move to device here
+    if fixed_vae:
+        vae = AutoencoderKL.from_pretrained(vae_model_id, torch_dtype=dtype).to(device)
     else:
-        vae = AutoencoderKL.from_pretrained(model_id,
-                                            subfolder="vae",
-                                            torch_dtype=dtype,
-        ).to(device) # Move to device here
+        vae = AutoencoderKL.from_pretrained(
+            model_id, subfolder="vae", torch_dtype=dtype
+        ).to(device)
 
     UNet_Encoder = UNet2DConditionModel_ref.from_pretrained(
         model_id,
         subfolder="unet_encoder",
         torch_dtype=dtypeQuantize,
-    ).to(device) # Move to device here
-    if load_mode == '4bit':
-        quantize_4bit(UNet_Encoder)
+    ).to(device)
+    if load_mode == '8bit':
+        quantize_8bit(UNet_Encoder)
 
     UNet_Encoder.requires_grad_(False)
     image_encoder.requires_grad_(False)
@@ -82,32 +88,33 @@ def load_models():
     unet.requires_grad_(False)
 
     pipe_param = {
-            'pretrained_model_name_or_path': model_id,
-            'unet': unet,
-            'torch_dtype': dtype,
-            'vae': vae,
-            'image_encoder': image_encoder,
-            'feature_extractor': CLIPImageProcessor(),
-        }
+        'pretrained_model_name_or_path': model_id,
+        'unet': unet,
+        'torch_dtype': dtype,
+        'vae': vae,
+        'image_encoder': image_encoder,
+        'feature_extractor': CLIPImageProcessor(),
+    }
 
     pipe = TryonPipeline.from_pretrained(**pipe_param).to(device)
     pipe.unet_encoder = UNet_Encoder
     pipe.unet_encoder.to(pipe.unet.device)
 
-    if load_mode == '4bit':
+    if load_mode == '8bit':
         if pipe.text_encoder is not None:
-            quantize_4bit(pipe.text_encoder)
-            pipe.text_encoder.to(device) # Move to device
+            quantize_8bit(pipe.text_encoder)
+            pipe.text_encoder.to(device)
         if pipe.text_encoder_2 is not None:
-            quantize_4bit(pipe.text_encoder_2)
-            pipe.text_encoder_2.to(device) # Move to device
+            quantize_8bit(pipe.text_encoder_2)
+            pipe.text_encoder_2.to(device)
 
-    # Initialize other models here as well
     parsing_model = Parsing(0)
     openpose_model = OpenPose(0)
     openpose_model.preprocessor.body_estimation.model.to(device)
-    print("loading models completed!!!!!!!!!!!!")
 
+    gc.collect()
+    torch.cuda.empty_cache()
+    print("loading models completed!!!!!!!!!!!!")
 
 # Your API endpoints and other Flask code here...
 from flask import request, jsonify
@@ -118,7 +125,7 @@ from util.common import open_folder
 from util.image import pil_to_binary_mask, save_output_image
 from utils_mask import get_mask_location
 import apply_net
-from detectron2.data.detection_utils import convert_PIL_to_numpy,_apply_exif_orientation
+from detectron2.data.detection_utils import convert_PIL_to_numpy, _apply_exif_orientation
 from torchvision.transforms.functional import to_pil_image
 
 class TryonRequest(BaseModel):
@@ -135,14 +142,14 @@ class TryonRequest(BaseModel):
 
 @app.route('/tryon', methods=['POST'])
 def tryon_image():
-    global pipe, parsing_model, openpose_model, tensor_transfrom # Access the globally loaded models
+    global pipe, parsing_model, openpose_model, tensor_transfrom  # Access the globally loaded models
 
     # Process request
     data = request.get_json()
     human_image = Image.open(data['human_image'])
     garment_image = Image.open(data['garment_image'])
 
-    garm_img= garment_image.convert("RGB").resize((768,1024))
+    garm_img = garment_image.convert("RGB").resize((768, 1024))
     human_img_orig = human_image.convert("RGB")
 
     if data['is_checked_crop']:
@@ -155,28 +162,30 @@ def tryon_image():
         bottom = (height + target_height) / 2
         cropped_img = human_img_orig.crop((left, top, right, bottom))
         crop_size = cropped_img.size
-        human_img = cropped_img.resize((768,1024))
+        human_img = cropped_img.resize((768, 1024))
     else:
-        human_img = human_img_orig.resize((768,1024))
+        human_img = human_img_orig.resize((768, 1024))
 
     if data['is_checked']:
-        keypoints = openpose_model(human_img.resize((384,512)))
-        model_parse, _ = parsing_model(human_img.resize((384,512)))
+        keypoints = openpose_model(human_img.resize((384, 512)))
+        model_parse, _ = parsing_model(human_img.resize((384, 512)))
         mask, mask_gray = get_mask_location('hd', data['category'], model_parse, keypoints)
-        mask = mask.resize((768,1024))
+        mask = mask.resize((768, 1024))
     else:
         mask = pil_to_binary_mask(human_image.convert("RGB").resize((768, 1024)))
 
-    mask_gray = (1-transforms.ToTensor()(mask)) * tensor_transfrom(human_img)
-    mask_gray = to_pil_image((mask_gray+1.0)/2.0)
+    mask_gray = (1 - transforms.ToTensor()(mask)) * tensor_transfrom(human_img)
+    mask_gray = to_pil_image((mask_gray + 1.0) / 2.0)
 
-    human_img_arg = _apply_exif_orientation(human_img.resize((384,512)))
+    human_img_arg = _apply_exif_orientation(human_img.resize((384, 512)))
     human_img_arg = convert_PIL_to_numpy(human_img_arg, format="BGR")
 
-    args = apply_net.create_argument_parser().parse_args(('show', './configs/densepose_rcnn_R_50_FPN_s1x.yaml', './ckpt/densepose/model_final_162be9.pkl', 'dp_segm', '-v', '--opts', 'MODEL.DEVICE', 'cuda'))
-    pose_img = args.func(args,human_img_arg)
-    pose_img = pose_img[:,:,::-1]
-    pose_img = Image.fromarray(pose_img).resize((768,1024))
+    args = apply_net.create_argument_parser().parse_args(
+        ('show', './configs/densepose_rcnn_R_50_FPN_s1x.yaml', './ckpt/densepose/model_final_162be9.pkl', 'dp_segm', '-v', '--opts', 'MODEL.DEVICE', 'cuda')
+    )
+    pose_img = args.func(args, human_img_arg)
+    pose_img = pose_img[:, :, ::-1]
+    pose_img = Image.fromarray(pose_img).resize((768, 1024))
 
     with torch.no_grad():
         # Extract the images
@@ -216,8 +225,8 @@ def tryon_image():
                             negative_prompt=negative_prompt,
                         )
 
-                    pose_img =  tensor_transfrom(pose_img).unsqueeze(0).to(device,torch.float16)
-                    garm_tensor =  tensor_transfrom(garm_img).unsqueeze(0).to(device,torch.float16)
+                    pose_img = tensor_transfrom(pose_img).unsqueeze(0).to(device, torch.float16)
+                    garm_tensor = tensor_transfrom(garm_img).unsqueeze(0).to(device, torch.float16)
                     results = []
                     current_seed = data['seed']
                     for i in range(data['number_of_images']):
@@ -227,21 +236,21 @@ def tryon_image():
                         current_seed = current_seed + i
 
                         images = pipe(
-                            prompt_embeds=prompt_embeds.to(device,torch.float16),
-                            negative_prompt_embeds=negative_prompt_embeds.to(device,torch.float16),
-                            pooled_prompt_embeds=pooled_prompt_embeds.to(device,torch.float16),
-                            negative_pooled_prompt_embeds=negative_pooled_prompt_embeds.to(device,torch.float16),
+                            prompt_embeds=prompt_embeds.to(device, torch.float16),
+                            negative_prompt_embeds=negative_prompt_embeds.to(device, torch.float16),
+                            pooled_prompt_embeds=pooled_prompt_embeds.to(device, torch.float16),
+                            negative_pooled_prompt_embeds=negative_pooled_prompt_embeds.to(device, torch.float16),
                             num_inference_steps=data['denoise_steps'],
                             generator=generator,
-                            strength = 1.0,
-                            pose_img = pose_img.to(device,torch.float16),
-                            text_embeds_cloth=prompt_embeds_c.to(device,torch.float16),
-                            cloth = garm_tensor.to(device,torch.float16),
+                            strength=1.0,
+                            pose_img=pose_img.to(device, torch.float16),
+                            text_embeds_cloth=prompt_embeds_c.to(device, torch.float16),
+                            cloth=garm_tensor.to(device, torch.float16),
                             mask_image=mask,
                             image=human_img,
                             height=1024,
                             width=768,
-                            ip_adapter_image = garm_img.resize((768,1024)),
+                            ip_adapter_image=garm_img.resize((768, 1024)),
                             guidance_scale=2.0,
                             dtype=torch.float16,
                             device=device,
